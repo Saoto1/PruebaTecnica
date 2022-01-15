28bba9a75a15756777d4bdc59e609b54c5a428a0
Saul Valdez <saulvaldez472@gmail.com>
2022-01-15 05:31:18 +0100

    Se mapearon las entidades para la DB

---
+++ SalonBelleza.EntidadesDeNegocio/Libros.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace PruebaTecnica.EntidadesDeNegocio
+{
+   public class Libros
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El DUI obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Titulo { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre del cliente es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Autor { get; set; }
+
+
+        [Required(ErrorMessage = "El apellido del cliente es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public DateTime FechaPublicacion { get; set; }
+
+
+        [Required(ErrorMessage = "El Teléfono es obligatorio")]
+        [Display(Name = "Teléfono")]
+        public int Genero { get; set; }
+
+        [Required(ErrorMessage = "El Teléfono es obligatorio")]
+        [Display(Name = "Teléfono")]
+        public int Estado { get; set; }
+
+
+    }
+}
