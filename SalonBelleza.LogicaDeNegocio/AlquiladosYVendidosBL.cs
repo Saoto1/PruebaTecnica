7b9cfaf3e8e243a2990e68b995c35b693bda735c
Saul Valdez <saulvaldez472@gmail.com>
2022-01-15 22:13:11 +0100

    BL terminadas

---
+++ SalonBelleza.LogicaDeNegocio/AlquiladosYVendidosBL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PruebaTecnica.EntidadesDeNegocio;
+using PruebaTecnica.AccesoADatos;
+
+namespace PruebaTecnica.LogicaDeNegocio
+{
+   public class AlquiladosYVendidosBL
+    {
+
+        public async Task<int> CrearAsync(AlquiladosYVendidos pAlquiladosYVendidos)
+        {
+            return await AlquiladosYVendidosDAL.CrearAsync(pAlquiladosYVendidos);
+        }
+
+        public async Task<int> ModificarAsync(AlquiladosYVendidos pAlquiladosYVendidos)
+        {
+            return await AlquiladosYVendidosDAL.ModificarAsync(pAlquiladosYVendidos);
+        }
+        public async Task<int> EliminarAsync(AlquiladosYVendidos pAlquiladosYVendidos)
+        {
+            return await AlquiladosYVendidosDAL.EliminarAsync(pAlquiladosYVendidos);
+        }
+        public async Task<AlquiladosYVendidos> ObtenerPorIdAsync(AlquiladosYVendidos pAlquiladosYVendidos)
+        {
+            return await AlquiladosYVendidosDAL.ObtenerPorIdAsync(pAlquiladosYVendidos);
+        }
+
+        public async Task<List<AlquiladosYVendidos>> BuscarAsync(AlquiladosYVendidos pAlquiladosYVendidos)
+        {
+            return await AlquiladosYVendidosDAL.BuscarAsync(pAlquiladosYVendidos);
+        }
+
+    }
+}
---
+++ SalonBelleza.LogicaDeNegocio/LibrosBL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PruebaTecnica.EntidadesDeNegocio;
+using PruebaTecnica.AccesoADatos;
+namespace PruebaTecnica.LogicaDeNegocio
+{
+  public  class LibrosBL
+    {
+
+        public async Task<int> CrearAsync(Libros pLibros)
+        {
+            return await LibrosDAL.CrearAsync(pLibros);
+        }
+
+        public async Task<int> ModificarAsync(Libros pLibros)
+        {
+            return await LibrosDAL.ModificarAsync(pLibros);
+        }
+        public async Task<int> EliminarAsync(Libros pLibros)
+        {
+            return await LibrosDAL.EliminarAsync(pLibros);
+        }
+        public async Task<Libros> ObtenerPorIdAsync(Libros pLibros)
+        {
+            return await LibrosDAL.ObtenerPorIdAsync(pLibros);
+        }
+
+        public async Task<List<Libros>> BuscarAsync(Libros pLibros)
+        {
+            return await LibrosDAL.BuscarAsync(pLibros);
+        }
+
+    }
+}
